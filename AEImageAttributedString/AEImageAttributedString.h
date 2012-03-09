3447f0459c698b374c539ca98591ae7d6b0dfcbc
Adam Ernst <adamjernst@me.com>
2012-03-08 19:21:29 -0500

    Finish implementation.

--- AEImageAttributedString/AEImageAttributedString.h
+++ AEImageAttributedString/AEImageAttributedString.h
@@ -13,9 +13,27 @@
 
 + (NSAttributedString *)attributedStringWithImage:(UIImage *)image;
 
-// For the image to actually appear when you draw the NSAttributedString,
-// you MUST call +drawImages AFTER creating the frame but BEFORE actually
-// drawing the frame (since drawing invalidates the frame).
+// If you don't want to mess with Core Text, here are two convenience methods.
+//   - They use UIGraphicsGetCurrentContext() to get the context to draw in
+//   - They assume you're using flipped UIKit coordinates (the default).
+//   - +drawAttributedString:inRect: calls +drawImagesForFrame:fromAttributedString:
+//     for you.
++ (CGSize)sizeForAttributedString:(NSAttributedString *)str constrainedToSize:(CGSize)dimensions;
++ (void)drawAttributedString:(NSAttributedString *)str inRect:(CGRect)rect;
+
+// Calling sizeForAttributedString followed by drawAttributedString is somewhat
+// wasteful; it creates a framesetter twice. If you want to be more efficient:
+//   1. Create the framesetter yourself with CTFramesetterCreateWithAttributedString
+//   2. Get the ideal size from CTFramesetterSuggestFrameSizeWithConstraints
+//      (sizeForAttributedString just wraps this function)
+//   3. Call drawAttributedString:framesetter:inRect: to do the actual drawing.
+//      (The NSAttributedString is only needed for drawImagesForFrame:.)
++ (void)drawAttributedString:(NSAttributedString *)str framesetter:(CTFramesetterRef)framesetter inRect:(CGRect)rect;
+
+// If you ARE using Core Text directly, for the image to actually appear when
+// you draw the NSAttributedString, you MUST call 
+// +drawImagesForFrame:fromAttributedString: AFTER creating the frame but 
+// BEFORE actually drawing the frame (since drawing invalidates the frame).
 + (void)drawImagesForFrame:(CTFrameRef)frame fromAttributedString:(NSAttributedString *)string;
 
 @end
--- AEImageAttributedString/AEImageAttributedString.m
+++ AEImageAttributedString/AEImageAttributedString.m
@@ -24,6 +24,8 @@ CGFloat AEImageAttributedStringRunDelegateWidthCallback(void *refCon) {
     return [(UIImage *)refCon size].width;
 }
 
+static NSString *kAEImageAttributeName = @"kAEImageAttributeName";
+
 @implementation AEImageAttributedString
 
 + (NSAttributedString *)attributedStringWithImage:(UIImage *)image {
@@ -35,45 +37,82 @@ CGFloat AEImageAttributedStringRunDelegateWidthCallback(void *refCon) {
         .getWidth = AEImageAttributedStringRunDelegateWidthCallback,
     };
     CTRunDelegateRef runDelegate = CTRunDelegateCreate(&callbacks, [image retain]);
-    NSDictionary *attribs = [NSDictionary dictionaryWithObject:(id)runDelegate forKey:(id)kCTRunDelegateAttributeName];
+    NSDictionary *attribs = [NSDictionary dictionaryWithObjectsAndKeys:
+                             (id)runDelegate, (id)kCTRunDelegateAttributeName,
+                             image, kAEImageAttributeName, nil];
     NSAttributedString *r = [[[NSAttributedString alloc] initWithString:@"\uFFFC" attributes:attribs] autorelease];
     CFRelease(runDelegate);
     return r;
+}
+
++ (CGSize)sizeForAttributedString:(NSAttributedString *)str constrainedToSize:(CGSize)dimensions {
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)str);
+    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, 0), NULL, dimensions, NULL);
+    CFRelease(framesetter);
+    return suggestedSize;
+}
+
++ (void)drawAttributedString:(NSAttributedString *)str inRect:(CGRect)rect {
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)str);
+    [AEImageAttributedString drawAttributedString:str framesetter:framesetter inRect:rect];
+    CFRelease(framesetter);
+}
+
++ (void)drawAttributedString:(NSAttributedString *)str framesetter:(CTFramesetterRef)framesetter inRect:(CGRect)rect {
+    CGRect zeroYRect = rect;
+    zeroYRect.origin.y = 0.0f;
+    CGPathRef path = CGPathCreateWithRect(zeroYRect, NULL);
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, NULL);
+    CFRelease(path);
     
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    CGContextSetTextMatrix(c, CGAffineTransformIdentity);
+    CGContextSaveGState(c);
+    // Flip the CTM for iOS style top-left origin.
+    CGContextConcatCTM(c, CGAffineTransformMake(1.0, 0.0, 0.0, -1.0, 0.0, rect.origin.y + rect.size.height));
+    [AEImageAttributedString drawImagesForFrame:frame fromAttributedString:str];
+    CTFrameDraw(frame, c);
+    CGContextRestoreGState(c);
+    
+    CFRelease(frame);
 }
 
 + (void)drawImagesForFrame:(CTFrameRef)frame fromAttributedString:(NSAttributedString *)string {
-    /*CGRect rect = CGPathGetBoundingBox(CTFrameGetPath(frame));
-    [string enumerateAttribute:(id)kCTRunDelegateAttributeName 
+    CGRect rect = CGPathGetBoundingBox(CTFrameGetPath(frame));
+    CFArrayRef lines = CTFrameGetLines(frame);
+    
+    // Flip the CTM. We assume that the CTM is *currently* the Core Text/Mac 
+    // style, with the origin in bottom left. UIImage uses a top left origin.
+    // If we don't flip the CTM, images will be drawn upside down.
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(c);
+    CGContextConcatCTM(c, CGAffineTransformMake(1.0, 0.0, 0.0, -1.0, 0.0, rect.origin.y + rect.size.height));
+    
+    [string enumerateAttribute:(id)kAEImageAttributeName 
                        inRange:NSMakeRange(0, [string length]) 
                        options:0
                     usingBlock:^(id value, NSRange range, BOOL *stop) {
-                        UIImage *image = (UIImage *) CTRunDelegateGetRefCon((CTRunDelegateRef)value);
-                        // Attempt to get the offset for this range in the string.
-                        CFArrayRef lines = CTFrameGetLines(frame);
+                        UIImage *image = (UIImage *) value;
                         
-                        CGFloat xOffset = 0.0f;
-                        CGFloat yOffset = 0.0f;
+                        CGFloat x = 0.0f;
+                        CGFloat y = 0.0f;
                         
                         for (CFIndex i = 0; i < CFArrayGetCount(lines); i++) {
                             CTLineRef line = CFArrayGetValueAtIndex(lines, i);
                             CFRange r = CTLineGetStringRange(line);
                             int localIndex = range.location - r.location;
                             if (localIndex >= 0 && localIndex < r.length) {
-                                xOffset = CTLineGetOffsetForStringIndex(line, range.location, NULL);
+                                x = CTLineGetOffsetForStringIndex(line, range.location, NULL);
                                 CGPoint lineOrigin;
                                 CTFrameGetLineOrigins(frame, CFRangeMake(i, 1), &lineOrigin);
-                                // Flip coordinate system from CoreText's 0-on-bottom to
-                                // UIKit's 0-on-top. This assumes you're flipping the context
-                                // when you draw the CoreText frame.
-                                yOffset = height - lineOrigin.y;
+                                y = lineOrigin.y;
                                 break;
                             }
                         }
-                        [image drawAtPoint:CGPointMake(rect.origin.x + xOffset, rect.origin.y + yOffset - [image size].height + 2.0f)];
-                    }];*/
-// TODO incomplete
-// TODO use separate attr for image to prevent crashing on other run delegates
+                        [image drawAtPoint:CGPointMake(rect.origin.x + x, rect.size.height - y - [image size].height + 2.0f)];
+                    }];
+    
+    CGContextRestoreGState(c);
 }
 
 @end
